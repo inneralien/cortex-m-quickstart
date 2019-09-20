88549f139c21749470fdcc0eb8c6bbe5739bcc55
Tim Weaver <inneralien@gmail.com>
2019-09-19 19:35:27 -0700

    Update base template for stm32f4

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,15 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::asm;
+use cortex_m::{iprintln, Peripherals};
 use cortex_m_rt::entry;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+    let mut p = Peripherals::take().unwrap();
+    let stim = &mut p.ITM.stim[0];
+    iprintln!(stim, "Stellarbourne Broadcast System");
 
-    loop {
-        // your code goes here
-    }
+    p.GPIOA.odr.write(|w|, w.bits(0x3));
+    loop {}
 }
