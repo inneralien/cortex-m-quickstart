f7720aa3de43851e4c6af3d7dd5c15743264142c
Tim Weaver <inneralien@gmail.com>
2019-09-22 20:17:36 -0700

    Configure peripherals to light LED LD2.
    Very basic configuration, but shows what bits are required to enable the
    peripheral bus where GPIOA is located. On the Nucleo-F7411RE board the
    green LED is LD2 and is on GPIOA port 5.

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,27 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 // extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::{iprintln, Peripherals};
+use cortex_m::{iprintln}; //, Peripherals};
 use cortex_m_rt::entry;
+use stm32f4::stm32f411;
 
 #[entry]
 fn main() -> ! {
-    let mut p = Peripherals::take().unwrap();
-    let stim = &mut p.ITM.stim[0];
+    let peripherals = stm32f411::Peripherals::take().unwrap();
+    let mut cp = stm32f411::CorePeripherals::take().unwrap();
+    let stim = &mut cp.ITM.stim[0];
     iprintln!(stim, "Stellarbourne Broadcast System");
 
-    p.GPIOA.odr.write(|w|, w.bits(0x3));
+    // Power up the AHB1 peripherals where the GPIO are connected
+    peripherals.RCC.ahb1enr.write(|w| w.gpioaen().set_bit());
+
+    // Configure PA5 as push-pull output pin.
+    // LED LD2 on Nucleo-F411RE dev board.
+    peripherals.GPIOA.otyper.write(|w| w.ot5().push_pull());
+    peripherals.GPIOA.moder.write(|w| w.moder5().output());
+    peripherals.GPIOA.pupdr.write(|w| w.pupdr5().floating());
+
+    peripherals.GPIOA.odr.write(|w| w.odr5().high());
+
     loop {}
 }
